ae059abb33e5675a1a2f3ce57578713be67088b2
IndraNaik <pathlavath.indra@gmail.com>
2025-02-21 12:10:54 +0530

    booking table and assign waiter

--- src/main/java/com/hotel_mgnt/_7/service/BookingTableService.java
+++ src/main/java/com/hotel_mgnt/_7/service/BookingTableService.java
@@ -1,4 +1,10 @@
 package com.hotel_mgnt._7.service;
 
+import com.hotel_mgnt._7.dto.BookingTableDto;
+import com.hotel_mgnt._7.exceptions.Response;
+
 public interface BookingTableService {
+    Response<BookingTableDto> bookTable(BookingTableDto bookingTableDto);
+
+    Response<BookingTableDto> assignWaiter(BookingTableDto bookingTableDto);
 }
